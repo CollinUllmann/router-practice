686b9cb862c8245a0bd3924d37be165f6372032f
Collin <collinullmann@gmail.com>
2024-01-09 15:56:39 -0500

    practice done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
 import Home from './components/Home';
 import Stocks from './components/Stocks';
 import Movies from './components/Movies';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+const router = createBrowserRouter(
+  [{
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/movies',
+    element: <Movies />
+  },
+  {
+    path: '/stocks',
+    element: <Stocks />
+  },
+  {
+    path: '*',
+    element: <h1>Page Not Found</h1>
+  }]
+);
 
 function App() {
   return (
     <div className='app'>
       <h1>App Component</h1>
-      <Home />
-      <Stocks />
-      <Movies />
+      <RouterProvider router={router} /> 
     </div>
   );
 }
 
 export default App;
+
+
